55a8c19444ab2eab24fd6d86393030183fc7ed79
Rakesh Shrestha <rakesh_shresthanp@yahoo.com>
2026-01-18 08:43:11 +0545

    upgrade dependencies

--- nodejs_aws_lamdas_dynamodb/src/utils/dynamoDB-mapper.ts
+++ nodejs_aws_lamdas_dynamodb/src/utils/dynamoDB-mapper.ts
@@ -6,7 +6,10 @@ dotenv.config();
 
 const dynamoDBOfflineOptions: DynamoDB.ClientConfiguration = {
   region: "localhost",
-  endpoint: "http://localhost:5000/",
+  endpoint: "http://127.0.0.1:5000", // Changed to 127.0.0.1 for Windows stability
+  // These lines are CRITICAL to stop the UnrecognizedClientException
+  accessKeyId: "LOCALID123", 
+  secretAccessKey: "LOCALSECRET123"
 };
 
 const dynamoMapper = new DataMapper({
